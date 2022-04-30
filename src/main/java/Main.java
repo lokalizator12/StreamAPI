59d790d5cb2d555d16b37cee2d2154217bb3fa7f
lokalizator12 <83495533+lokalizator12@users.noreply.github.com>
2022-04-30 22:29:17 +0200

    Stream API

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,25 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list1 = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++) {
-            list1.add((int) (Math.random() * 1000));
-        }
+       List<Float> floatList = new ArrayList<>();
+       for (int i = 0; i < 30_000_000; i++)
+       { floatList.add((float)i);
+       }
+       long before = System.currentTimeMillis();
+       floatList.stream()
+               .map((in) ->Math.sin(0.2f + in / 5) * Math.cos(0.2f + in / 5) * Math.cos(0.4f + in / 2))
+               .collect(Collectors.toList());
 
-        List<Integer> list2 = filter(list1, (n) -> n > 0);
-        for (int g : list2) {
-            System.out.println(g);
-        }
-        System.out.println("-------------------------------------------------");
-
-        List<Integer> list3 = filter(list1, (a) -> a % 5 == 0);
-        for (int g : list3) {
-            System.out.println(g);
-        }
-
-
-    }
+       long after = System.currentTimeMillis();
+        System.out.println(after - before);
+                }
 
     public static List<Integer> filter(List<Integer> listNotFiltred, Predicate d) {
         List<Integer> listFiltred = new ArrayList<>();
